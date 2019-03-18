7543922bd9ba2744dab862e9de522fc1139af3a8
APP技術部-陳冠志 <bruce@mitake.com.tw>
2019-03-18 13:56:22 +0800

    Initial Commit

---
+++ TestBarView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestBarView
+//
+//  Created by APP技術部-陳冠志 on 2019/3/18.
+//  Copyright © 2019 APP技術部-陳冠志. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
