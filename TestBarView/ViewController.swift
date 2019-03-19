877130c69d5429f59f10210d301c5ae446e9538f
APP技術部-陳冠志 <bruce@mitake.com.tw>
2019-03-19 15:25:52 +0800

    初版Demo

--- TestBarView/ViewController.swift
+++ TestBarView/ViewController.swift
@@ -9,12 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var backImageView: UIImageView!
+    @IBOutlet weak var topImageView: UIImageView!
+    @IBOutlet weak var maskView: UIView!
+    
+    var numberX: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        topImageView.mask = maskView
     }
-
-
+    
+    
+    @IBAction func addBtnClicked(_ sender: Any) {
+        
+        let tmpNumberX = self.numberX
+        
+        numberX += (topImageView.frame.width*0.33)
+    
+        guard numberX <= topImageView.frame.width else {
+            self.numberX = tmpNumberX
+            return
+        }
+        
+        let newMaskViewFrame = CGRect(x: 0,
+                                      y: maskView.frame.origin.y,
+                                      width: numberX,
+                                      height: maskView.frame.height)
+        
+        maskView.frame = newMaskViewFrame
+        
+//        print("add mask: \(maskView.frame)")
+//        print("add mask: \(maskView.frame.midX) ,\(maskView.frame.minX), \(maskView.frame.maxX)\n\n")
+    }
+    
+    @IBAction func minusBtnClicked(_ sender: Any) {
+        
+        let tmpNumberX = self.numberX
+        
+        numberX -= (topImageView.frame.width*0.33)
+        
+        guard numberX >= 0 else {
+            self.numberX = tmpNumberX
+            return
+        }
+        
+        let newMaskViewFrame = CGRect(x: 0,
+                                      y: maskView.frame.origin.y,
+                                      width: numberX,
+                                      height: maskView.frame.height)
+        maskView.frame = newMaskViewFrame
+        
+//        print("min mask: \(maskView.frame)")
+//        print("min mask: \(maskView.frame.midX) ,\(maskView.frame.minX), \(maskView.frame.maxX)\n\n")
+    }
+    
 }
 
